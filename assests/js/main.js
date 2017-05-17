8a8e3a23ed15103a6d459e600900b99a64de0359
“Trinidad <tfuentesfuentealba@gmail.com>
2017-05-17 20:48:22 -0300

    creando listado tareas

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,7 +1,12 @@
 var caja = document.getElementById("caja");
+var contenedor = document.getElementById("contenedorTareas");
+
 
 var agregar = document.getElementById("agregar"); //llamando al elemento que realizara la funcion
-agregar.addEventListener("click", function (){ // al hacer click...
+agregar.addEventListener("click", agregarTareas);
+
+
+function agregarTareas(){ 
 
 	//creando elementos
 	var nuevoDiv = document.createElement("div");
@@ -9,10 +14,6 @@ agregar.addEventListener("click", function (){ // al hacer click...
 	var nuevoBoton = document.createElement("button");
 
 	var textoBoton = document.createTextNode("Guardar");
-
-	//Creando boton x
-	var eliminar = document.createElement("span");
-	eliminar.classList.add("fa", "fa-times");	//icono de basurero
 
 	//Asignando clases y atributos
 	nuevoDiv.setAttribute("id", "cont")
@@ -25,28 +26,42 @@ agregar.addEventListener("click", function (){ // al hacer click...
 	nuevoBoton.appendChild(textoBoton);
 	nuevoDiv.appendChild(nuevoInput);
 	nuevoDiv.appendChild(nuevoBoton);
-	nuevoDiv.appendChild(eliminar);
 
 
 	//Asignandole el titulo a las tareas
-	
 	var guardar = document.getElementById("guardar");
+
 	guardar.addEventListener("click", function(){
+
 		var tareas = document.getElementById("tareas").value;
-	
+		document.getElementById("tareas").value = "";	// Limpiando textarea
 
 		if(tareas == null || tareas.length == 0){
-		alert("Tienes que ingresar algo");
-		}else{
-		document.getElementById("agregar").textContent = tareas; //Cambiandole el texto del a con el titulo que se asigno
-		}
-	})
 
-	eliminar.addEventListener("click", function(){
-			nuevoDiv.removeChild(tareas);
-	})
-	
-});
+			alert("Tienes que ingresar algo");
+			}
+
+			else{
+			/*document.getElementById("agregar").textContent = tareas; //Cambiandole el texto del a con el titulo que se asigno*/
+			var	nuevoParrafo = document.createElement("p");
+			var nuevoParrafoTexto = document.createTextNode(tareas);
+
+			nuevoParrafo.appendChild(nuevoParrafoTexto);
+			contenedor.appendChild(nuevoParrafo);
+
+			//Creando boton x
+			var eliminar = document.createElement("span");
+			eliminar.classList.add("fa", "fa-times");	//icono de basurero
+			nuevoParrafo.appendChild(eliminar);
+			}
+
+			eliminar.addEventListener("click", function(){
+			contenedor.removeChild(nuevoParrafo);
+			})
+
+		})
+
+};
 
 /* //Intentando ocultar cajita
 var contenedor = document.getElementById("cont");
